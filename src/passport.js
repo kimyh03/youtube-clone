2a83119cd93e8234fc6ed7667b7c56ac371c15fe
Hoony <59421544+kimyh03@users.noreply.github.com>
2020-03-27 20:35:06 +0900

    debug passport

--- src/passport.js
+++ src/passport.js
@@ -12,7 +12,7 @@ passport.use(
       clientID: process.env.GH_ID,
       clientSecret: process.env.GH_SECRET,
       callbackURL: process.env.PRODUCTION
-        ? `https://young-headland-33416.herokuapp.com/${routes.githubCallback}`
+        ? `https://young-headland-33416.herokuapp.com${routes.githubCallback}`
         : `http://localhost:4000${routes.githubCallback}`
     },
     githubLoginCallback
